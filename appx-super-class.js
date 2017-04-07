c6d421ed54b1d99256a8081acd1b764a3516c57a
khai96_ <hvksmr1996@gmail.com>
2017-04-07 21:17:52 +0700

    Unused variable

--- appx-super-class.js
+++ appx-super-class.js
@@ -1,6 +1,4 @@
 'use strict'
-
-const createClassFromSuper = require('simple-class-utils').createClass.super
 const {XIterable, Root} = require('x-iterable-base')
 
 module.exports = (build, iterate) => {

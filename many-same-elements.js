6afc77fbbba45957ad1674ce08259f0a0f9a1d59
khai96_ <hvksmr1996@gmail.com>
2017-04-07 21:13:46 +0700

    Add little more spaces

--- many-same-elements.js
+++ many-same-elements.js
@@ -2,15 +2,19 @@
 
 module.exports = (value, times) => {
   times = parseInt(times)
+
   if (times < 0 || isNaN(times)) {
     throw new RangeError(`Parameter 'times' is invalid`)
   }
+
   if (!times) {
     return []
   }
+
   var result = []
   for (; times; --times) {
     result.push(value)
   }
+
   return result
 }
